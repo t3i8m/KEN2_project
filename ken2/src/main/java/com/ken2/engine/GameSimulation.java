06ea14a01538453e71cbbf33d5975d4606e6679c
Mona Lisa <MonaLisa@email>
2024-09-25 10:22:18 +0200

    returned comments back

--- ken2/src/main/java/com/ken2/engine/GameSimulation.java
+++ ken2/src/main/java/com/ken2/engine/GameSimulation.java
@@ -6,21 +6,32 @@ import com.ken2.Game_Components.Board.Game_Board;
 import com.ken2.Game_Components.Board.Vertex;
 import com.ken2.engine.Diagonal;
 
-
+/**
+ * Class to simulate all possible moves from a given disk position
+ */
 public class GameSimulation {
 
     private ArrayList<ArrayList> allPossibleMoves;
 
+    /**
+     * Initializing GameSimulation class
+     */
     public GameSimulation(){
         allPossibleMoves= new ArrayList<ArrayList>();
     }
 
+    /**
+     * Method to simulate all the moves from a given position
+     * 
+     * @param board board of the game 2d array of vertices
+     * @param xDiskPosition x position of the disk
+     * @param yDiskPosition y position of the disk
+     */
+
     // instead of the x&y distances we will use disk obj and its atributes
-    public void startSimulation(Game_Board boardObj, int xDiskPosition, int yDiskPosition){
+    public void startSimulation(Vertex[][] board, int xDiskPosition, int yDiskPosition){
 
         int[] diskPositions= {xDiskPosition, yDiskPosition};
-        Vertex[][] board = boardObj.getBoard();
-        // Vertex currVertex = board[xDiskPosition][yDiskPosition];
 
         for (Direction direction : Direction.values()) {
             Diagonal currDiagonal= new Diagonal(direction, diskPositions);
@@ -28,6 +39,11 @@ public class GameSimulation {
         }
     }
 
+    /**
+     * getter for the possible moves
+     * 
+     * @return ArrayList with all of the possible moves
+     */
     public ArrayList<ArrayList> getAllPossibleMoves(){
         return allPossibleMoves;
     }
