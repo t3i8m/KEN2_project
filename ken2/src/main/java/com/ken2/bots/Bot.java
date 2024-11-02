4ccc3fe4022d8dcfe664cc17ac3880b1e0f67169
t3i8m <number.38.tj@gmail.com>
2024-11-02 22:45:08 +0100

    added abstract classes and bot factory

---
+++ ken2/src/main/java/com/ken2/bots/Bot.java
@@ -0,0 +1,10 @@
+package com.ken2.bots;
+
+import com.ken2.Game_Components.Board.Game_Board;
+import com.ken2.engine.Move;
+
+// interface for all of the bots
+public interface Bot {
+    String getColor();
+
+}
