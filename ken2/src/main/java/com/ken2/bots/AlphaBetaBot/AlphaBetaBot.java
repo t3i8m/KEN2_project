3f992ca599871ef07bb31f9ff44a79a2c9f3924d
t3i8m <number.38.tj@gmail.com>
2024-11-14 11:35:24 +0100

    introduced alpha beta clas

---
+++ ken2/src/main/java/com/ken2/bots/AlphaBetaBot/AlphaBetaBot.java
@@ -0,0 +1,21 @@
+package com.ken2.bots.AlphaBetaBot;
+
+
+import com.ken2.bots.BotAbstract;
+import com.ken2.engine.Move;
+
+public class AlphaBetaBot extends BotAbstract{
+
+    public AlphaBetaBot(String color){
+        super(color);
+    }
+
+    public void makeMove(){
+
+    }
+
+    public void alphaBetaBot(){
+
+    }
+
+}
