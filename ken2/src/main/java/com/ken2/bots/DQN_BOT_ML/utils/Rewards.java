44ba723d23d15a95f146a5b5d8f173ba8cd7b824
Valerija Krumplevskaja <88630777+laska8@users.noreply.github.com>
2025-01-09 20:57:02 +0100

    Update Rewards.java

--- ken2/src/main/java/com/ken2/bots/DQN_BOT_ML/utils/Rewards.java
+++ ken2/src/main/java/com/ken2/bots/DQN_BOT_ML/utils/Rewards.java
@@ -1,18 +1,25 @@
 package com.ken2.bots.DQN_BOT_ML.utils;
 
 public enum Rewards {
-    WIN(10),
+    WIN(50),
     CHIPS_IN_A_ROW(4),
+    THREE_IN_A_ROW(2),
+    FOUR_IN_A_ROW(3),
     FIVE_CHIPS_IN_A_ROW(6),
     YOUR_RING_REMOVAL(10),
     OPPONENT_RING_REMOVAL(-10),
-    SUCCESSSFUL_MOVE(1),
+    SUCCESSFUL_MOVE(1),
+    COMPLEX_MOVE (5),///пересечение коинов
+    DOUBLE_ROW_CREATION (15),
+    FLIP_MARKERS(1),
     INVALID_MOVE(-100),
+    SELF_BLOCK(-10),
     DRAW(5),
-    LOSE(-10);
+    LOSE(-10),
+    OPPONENT_ROW_CREATION(-20);
 
 
-    private final int value;
+    private int value;
 
    Rewards(int bonus){
     this.value = bonus;
@@ -21,4 +28,24 @@ public enum Rewards {
    public int getValue() {
     return this.value;
 }
+    public void setValue(int newValue) {
+        this.value = newValue;
+    }
+
+    public static int calculateReward(Rewards rewardType, int additionalFactor) {
+        return rewardType.getValue() + additionalFactor;
+    }
+
+    public static void logReward(Rewards rewardType, String context) {
+        System.out.println("Reward applied: " + rewardType.name() + " | Value: " + rewardType.getValue() + " | Context: " + context);
+    }
+
+    public static double normalizeReward(int reward) {
+        final int MIN_REWARD = -100;
+        final int MAX_REWARD = 50;
+        return (double)(reward - MIN_REWARD) / (MAX_REWARD - MIN_REWARD) * 2 - 1;
+    }
+
+
+
 }
