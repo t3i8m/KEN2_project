89ce57b72c59cbbb4a0830cd7b784ca6bac3b758
laska8 <lelera04@gmail.com>
2025-01-10 13:27:04 +0100

    Epsilon-greedy agorithm

---
+++ ken2/src/main/java/com/ken2/bots/DQN_BOT_ML/NeuralNetwork/neuralNetworkComponents/ActivationFunctions/LeakyReLu.java
@@ -0,0 +1,13 @@
+package com.ken2.bots.DQN_BOT_ML.NeuralNetwork.neuralNetworkComponents.ActivationFunctions;
+
+public class LeakyReLu implements ActivationFunction{
+    @Override
+    public double activate(double weightedSum) {
+        return 0;
+    }
+
+    @Override
+    public double derivative(double x) {
+        return 0;
+    }
+}
--- ken2/src/main/java/com/ken2/bots/DQN_BOT_ML/botComponents/DQN_BOT.java
+++ ken2/src/main/java/com/ken2/bots/DQN_BOT_ML/botComponents/DQN_BOT.java
@@ -15,25 +15,30 @@ public class DQN_BOT  extends BotAbstract{
     private double epsilonMin; // min epsilon for the egreedy
     private double epsilonDecay; // speed of the epsilon decrease
     private double gamma;  //discount for the Q update
-    private Random random = new Random();
+    private Random random;
+    private int actionSize;
+
 
     public DQN_BOT(String color){
         super(color);
         this.qNetwork = new NeuralNetwork(0.01, new MSE());
-        this.epsilon = 1.0;
-        this.epsilonMin = 0.1;
-        this.epsilonDecay = epsilonDecay;
-        this.gamma = gamma;
+        this.epsilon = 1;
+        this.epsilonMin = 0.01;
+        this.epsilonDecay = 0.995;
+        this.gamma = 0.99;
+        this.random = new Random();
+        this.actionSize = 10; //CAN BE ADJUSTED;
 
     }
 
     public DQN_BOT(String color,NeuralNetwork qNetwork, double epsilon, double epsilonMin, double epsilonDecay, double gamma) {
         super(color);
-
+        this.qNetwork = qNetwork;
         this.epsilon=epsilon;
         this.epsilonMin = epsilonMin;
         this.epsilonDecay=epsilonDecay;
         this.gamma = gamma;
+        this.random = new Random();
     }
 
     // TODO: implement bot makeMove, uses nn to find the best move in the given state
@@ -49,4 +54,25 @@ public class DQN_BOT  extends BotAbstract{
     public void train(int batchSize){
         // TODO: implement bot makeMove, uses nn to find the best move in the given state
     }
+
+    public int chooseAction(double[] qValues){
+        if(random.nextDouble() < epsilon){//если меньше, выбирается случайное действие (exploration)
+            return random.nextInt(actionSize);
+        }
+        else{
+            return argMax(qValues);//выбирается макс Q значение(эксплотация)
+        }
+    }
+    //finds an index with the highest q-value
+    public int argMax(double[] qValues){
+        int bestAction = 0;
+        for (int i =1; i <qValues.length;i++){
+            if(qValues[i]>qValues[bestAction]){
+                bestAction=i;
+            }
+        }
+        return bestAction;
+    }
+
+
 }
